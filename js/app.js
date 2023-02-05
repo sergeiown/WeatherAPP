f09c6eed6f308b994d2c2f435e4d27fe1c3d4fa0
Serhii Myshko <sergeiown@gmail.com>
2023-02-05 23:15:45 +0200

    add country selection option

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 const form = document.querySelector("#form");
+const select = document.querySelector("#country");
 const currentStatus = document.querySelector("#status");
 const updateTime = document.querySelector("#updateTime");
 const cookieTime = document.querySelector("#savedTime");
 const weatherIcon = document.querySelector("#weatherIcon");
 
+/* Read the list of countries and make select>option */
+fetch("../data/countries.json")
+  .then((response) => response.json())
+  .then((countries) => {
+    countries.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country["alpha-2"];
+      option.text = country.name;
+      select.add(option);
+    });
+  });
+
+/* Read the list of countries and make select>option */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const city = document.querySelector("#city").value.trim();
+  const country = select.value;
   const now = new Date();
   currentStatus.textContent = `Loading...`;
   updateTime.textContent = "";
@@ -20,20 +35,15 @@ form.addEventListener("submit", (event) => {
 
   if (cookie) {
     const cookieData = JSON.parse(cookie.split("=")[1]);
-    const cookieCity = cookieData.city;
     const cookieSavedTime = new Date(cookieData.time);
 
-    // console.log(cookie);
-    // console.log(cookieCity);
-    // console.log(cookieSavedTime);
-    // console.log(now);
-
     /* Check if stored forecast is for the same city and if it's not older than 1 minute */
-    if (cookieCity === city && now - cookieSavedTime < 60000) {
-      //   console.log(cookie);
-      //   console.log(
-      //     new Date(JSON.parse(cookie.split("=")[1]).time).toLocaleString()
-      //   );
+    if (
+      cookieData.city === city &&
+      cookieData.country === country &&
+      now - cookieSavedTime < 60000
+    ) {
+      console.log(cookie);
 
       currentStatus.textContent = cookieData.forecast;
       updateTime.textContent = `Saved data is shown`;
@@ -45,7 +55,11 @@ form.addEventListener("submit", (event) => {
 
   /* If forecast is not stored in cookies or is outdated, fetch it from the server */
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cf56fb7d05b9d81a18cb8aa28abe286a`
+    `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+      city
+    )},${encodeURIComponent(
+      country
+    )}&units=metric&appid=cf56fb7d05b9d81a18cb8aa28abe286a`
   )
     .then((response) => {
       if (response.ok) {
@@ -55,13 +69,18 @@ form.addEventListener("submit", (event) => {
       }
     })
     .then((data) => {
-      const forecast = data.weather[0].description;
+      console.log(data);
+
+      const forecast = `it's ${data.weather[0].description} in ${data.name}, ${data.sys.country}`;
       const icon = data.weather[0].icon;
+
       currentStatus.textContent = forecast;
       updateTime.textContent = `Last update: ${now.toLocaleString()}`;
       weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon">`;
+
       document.cookie = `forecast=${JSON.stringify({
         city,
+        country,
         forecast,
         icon,
         time: now,
