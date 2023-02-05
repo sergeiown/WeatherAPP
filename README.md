e774b514d83bd5a19a146d96959028822d57eacb
Serhii Myshko <sergeiown@gmail.com>
2023-02-05 15:43:29 +0200

    add a request to the API

---
+++ README.md
@@ -0,0 +1,9 @@
+# Search for movies App
+
+Search application for movies and TV series using The Open Movie Database API with AJAX.
+
+Search results are displayed ten per page.
+Page navigation is implemented.
+You can view detailed information about the selected movie or series.
+
+![screenshot](./img/screenshot.jpg)
---
+++ js/app.js
@@ -0,0 +1,73 @@
+const form = document.querySelector("#form");
+const currentStatus = document.querySelector("#status");
+const updateTime = document.querySelector("#updateTime");
+const cookieTime = document.querySelector("#savedTime");
+const weatherIcon = document.querySelector("#weatherIcon");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const city = document.querySelector("#city").value;
+  const now = new Date();
+  currentStatus.textContent = `Loading...`;
+  updateTime.textContent = "";
+  cookieTime.textContent = "";
+  weatherIcon.innerHTML = "";
+
+  /* Check if forecast is stored in cookies */
+  const cookie = document.cookie
+    .split("; ")
+    .find((item) => item.startsWith("forecast="));
+
+  if (cookie) {
+    const cookieData = JSON.parse(cookie.split("=")[1]);
+    const cookieCity = cookieData.city;
+    const cookieSavedTime = new Date(cookieData.time);
+
+    // console.log(cookie);
+    // console.log(cookieCity);
+    // console.log(cookieSavedTime);
+    // console.log(now);
+
+    /* Check if stored forecast is for the same city and if it's not older than 10 minutes */
+    if (cookieCity === city && now - cookieSavedTime < 600000) {
+      //   console.log(cookie);
+      //   console.log(
+      //     new Date(JSON.parse(cookie.split("=")[1]).time).toLocaleString()
+      //   );
+
+      currentStatus.textContent = cookieData.forecast;
+      updateTime.textContent = `Last update: ${now.toLocaleString()}`;
+      cookieTime.textContent = `Cookie time: ${cookieSavedTime.toLocaleString()}`;
+      weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${cookieData.icon}@2x.png" alt="Weather icon">`;
+      return;
+    }
+  }
+
+  /* If forecast is not stored in cookies or is outdated, fetch it from the server */
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cf56fb7d05b9d81a18cb8aa28abe286a`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("City not found");
+      }
+    })
+    .then((data) => {
+      const forecast = data.weather[0].description;
+      const icon = data.weather[0].icon;
+      currentStatus.textContent = forecast;
+      updateTime.textContent = `Last update: ${now.toLocaleString()}`;
+      weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon">`;
+      document.cookie = `forecast=${JSON.stringify({
+        city,
+        forecast,
+        icon,
+        time: now,
+      })}; max-age=600`;
+    })
+    .catch((error) => {
+      currentStatus.textContent = error.message;
+    });
+});
