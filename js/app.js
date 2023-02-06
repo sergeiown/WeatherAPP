b51d8936eb292a27c195ebf0716a9d1261f219a4
Serhii Myshko <sergeiown@gmail.com>
2023-02-06 02:18:17 +0200

    futher work with additional information

--- js/app.js
+++ js/app.js
@@ -10,10 +10,17 @@ const buttonColors = [
   "indigo",
   "violet",
 ];
+const currentTemp = document.querySelector("#currentTemp");
+const weatherIcon = document.querySelector("#weatherIcon");
 const currentStatus = document.querySelector("#status");
+const feelsLike = document.querySelector("#feelsLike");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const sunrise = document.querySelector("#sunrise");
+const sunset = document.querySelector("#sunset");
 const updateTime = document.querySelector("#updateTime");
 const cookieTime = document.querySelector("#savedTime");
-const weatherIcon = document.querySelector("#weatherIcon");
+const modal = document.querySelector(".modal");
 
 /* make the button visible enough for the user */
 let colorIndex = 0;
@@ -37,13 +44,22 @@ fetch("../data/countries.json")
 /* Read the list of countries and make select>option */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const city = document.querySelector("#city").value.trim();
   const country = select.value;
   const now = new Date();
-  currentStatus.textContent = `Loading...`;
+
+  modal.showModal();
+  currentTemp.textContent = "";
+  weatherIcon.innerHTML = "";
+  currentStatus.textContent = "";
+  feelsLike.textContent = "";
+  humidity.textContent = "";
+  wind.textContent = "";
+  sunrise.textContent = "";
+  sunset.textContent = "";
   updateTime.textContent = "";
   cookieTime.textContent = "";
-  weatherIcon.innerHTML = "";
 
   /* Check if forecast is stored in cookies */
   const cookie = document.cookie
@@ -62,10 +78,15 @@ form.addEventListener("submit", (event) => {
     ) {
       console.log(cookie);
 
-      currentStatus.textContent = cookieData.forecast;
-      updateTime.textContent = `Saved data is shown`;
-      cookieTime.textContent = `Last update: ${cookieSavedTime.toLocaleString()}`;
+      modal.close();
+
+      currentTemp.textContent = cookieData.temp;
       weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${cookieData.icon}@2x.png" alt="Weather icon">`;
+      currentStatus.textContent = cookieData.forecast;
+      feelsLike.textContent = `Feels like: ${cookieData.tempFeelsLike}`;
+      cookieTime.textContent = `Last update: ${cookieSavedTime.toLocaleString()}`;
+      updateTime.textContent = `Saved data is shown`;
+
       return;
     }
   }
@@ -82,7 +103,7 @@ form.addEventListener("submit", (event) => {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("City not found");
+        throw new Error("City not found!");
       }
     })
     .then((data) => {
@@ -90,20 +111,31 @@ form.addEventListener("submit", (event) => {
 
       const forecast = `it's ${data.weather[0].description} in ${data.name}, ${data.sys.country}`;
       const icon = data.weather[0].icon;
+      const temp = `${Math.round(data.main.temp)} °C`;
+      const tempFeelsLike = `${Math.round(data.main.feels_like)} °C`;
 
-      currentStatus.textContent = forecast;
-      updateTime.textContent = `Last update: ${now.toLocaleString()}`;
+      modal.close();
+
+      currentTemp.textContent = temp;
       weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon">`;
+      currentStatus.textContent = forecast;
+      feelsLike.textContent = `Feels like: ${tempFeelsLike}`;
+      updateTime.textContent = `Last update: ${now.toLocaleString()}`;
 
       document.cookie = `forecast=${JSON.stringify({
+        temp,
+        icon,
         city,
         country,
         forecast,
-        icon,
+        tempFeelsLike,
         time: now,
       })}; max-age=600`;
     })
     .catch((error) => {
-      currentStatus.textContent = error.message;
+      modal.textContent = error.message;
+      setTimeout(() => {
+        modal.close();
+      }, 3000);
     });
 });
