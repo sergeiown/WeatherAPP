103ec8c5b73415099b65b905d052f6ee4253f4f4
Serhii Myshko <sergeiown@gmail.com>
2023-02-09 21:25:51 +0200

    Add timeout for waiting for a response

--- js/forecast.js
+++ js/forecast.js
@@ -7,23 +7,31 @@ import getDomVariables from "./dom_variables.js";
 const variables = getDomVariables();
 
 export function getForecast(city, country, now) {
-  fetch(
+  /* Timeout for a response for 10 seconds and then generate an error */
+  const timeout = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("Request timed out. Please try again later."));
+    }, 10000);
+  });
+
+  /* Request to the api for the selected city and country */
+  const apiRequest = fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
       city
     )},${encodeURIComponent(
       country
     )}&units=metric&appid=cf56fb7d05b9d81a18cb8aa28abe286a`
-  )
+  );
+
+  /* Use "Promise.race" which allows to determine which promise will end first. */
+  Promise.race([timeout, apiRequest])
     .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
+      if (!response.ok) {
         throw new Error("City not found!");
       }
+      return response.json();
     })
     .then((data) => {
-      console.log(data);
-
       /* Fill the widget with data from api response. Save data to the cookie */
       fillWidgetWithApi(city, country, data, now);
 
@@ -36,9 +44,45 @@ export function getForecast(city, country, now) {
     .catch((error) => {
       /* show the error for 3 seconds */
       variables.modal.textContent = error.message;
-
       setTimeout(() => {
         variables.modal.close();
       }, 3000);
     });
 }
+
+// export function getForecast(city, country, now) {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+//       city
+//     )},${encodeURIComponent(
+//       country
+//     )}&units=metric&appid=cf56fb7d05b9d81a18cb8aa28abe286a`
+//   )
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error("City not found!");
+//       }
+//     })
+//     .then((data) => {
+//       console.log(data);
+
+//       /* Fill the widget with data from api response. Save data to the cookie */
+//       fillWidgetWithApi(city, country, data, now);
+
+//       /* Make forecast visible and close preloader */
+//       showWidget();
+
+//       /* Close preloader */
+//       variables.modal.close();
+//     })
+//     .catch((error) => {
+//       /* show the error for 3 seconds */
+//       variables.modal.textContent = error.message;
+
+//       setTimeout(() => {
+//         variables.modal.close();
+//       }, 3000);
+//     });
+// }
