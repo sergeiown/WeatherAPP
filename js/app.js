87b7d39242cc32a49e6a7cab08b325f0c2326eda
Serhii Myshko <sergeiown@gmail.com>
2023-02-23 16:00:03 +0200

    add additional button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-"use strict";
+'use strict';
 
-import { getDomVariables } from "./dom_variables.js";
-import { getSuggestions } from "./suggestions.js";
-import { getCountries } from "./country_list.js";
-import { clearForm } from "./form_clear.js";
-import { hideWidget } from "./widget_invisible.js";
-import { cycleButtonColors } from "./colored_button.js";
-import { clearWidget } from "./widget_clear.js";
-import { checkCookie } from "./cookie_check.js";
-import { getForecast } from "./forecast.js";
+import { getDomVariables } from './dom_variables.js';
+import { createGeoButton } from './createGeoButton.js';
+import { getSuggestions } from './suggestions.js';
+import { getCountries } from './country_list.js';
+import { clearForm } from './form_clear.js';
+import { hideWidget } from './widget_invisible.js';
+import { cycleButtonColors } from './colored_button.js';
+import { clearWidget } from './widget_clear.js';
+import { checkCookie } from './cookie_check.js';
+import { getForecast } from './forecast.js';
 
 const variables = getDomVariables();
 
@@ -21,34 +22,40 @@ getCountries();
 /* Style the form-submit button (change colors smoothly) */
 cycleButtonColors();
 
+createGeoButton();
+
 /* Get a new country list each time a new city name is entered */
-variables.cityName.addEventListener("focus", getCountries);
+variables.cityName.addEventListener('focus', getCountries);
+
+/* ----- obtaining a forecast based on the form data ----- */
 
 /* Waiting for the user to submit the form */
-variables.form.addEventListener("submit", (event) => {
-  event.preventDefault();
+variables.form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-  /* Show preloader after submit */
-  variables.modal.innerHTML = variables.preloader;
-  variables.modal.showModal();
+    /* Show preloader after submit */
+    variables.modal.innerHTML = variables.preloader;
+    variables.modal.showModal();
 
-  /* Hide the widget if it was opened earlier */
-  hideWidget();
+    /* Hide the widget if it was opened earlier */
+    hideWidget();
 
-  /* Save the form data and clear the widget */
-  const city = variables.cityName.value.toLowerCase().trim();
-  const country = variables.countrySelect.value;
-  const now = new Date();
-  clearWidget();
+    /* Save the form data and clear the widget */
+    const city = variables.cityName.value.toLowerCase().trim();
+    const country = variables.countrySelect.value;
+    const now = new Date();
+    clearWidget();
 
-  /* Check if forecast is stored in cookies and has the same city and if it's not older than 10 minutes. Show widget if everything is okay */
-  if (checkCookie(city, country, now)) {
-    return;
-  }
+    /* Check if forecast is stored in cookies and has the same city and if it's not older than 10 minutes. Show widget if everything is okay */
+    if (checkCookie(city, country, now)) {
+        return;
+    }
 
-  /* If forecast is not stored in cookies or is outdated, fetch it from the server API. Show widget if everything is okay or show error message */
-  getForecast(city, country, now);
+    /* If forecast is not stored in cookies or is outdated, fetch it from the server API. Show widget if everything is okay or show error message */
+    getForecast(city, country, now);
 });
 
+/* ----- obtaining a forecast based on geolocation data ----- */
+
 /* Clear form data */
-variables.clear.addEventListener("click", clearForm);
+variables.clear.addEventListener('click', clearForm);
--- js/dom_variables.js
+++ js/dom_variables.js
@@ -1,45 +1,47 @@
-"use strict";
+'use strict';
 
-const form = document.querySelector("#form");
-const cityName = document.querySelector("#city");
-const countrySelect = document.querySelector("#country");
-const button = document.querySelector("button[type=submit]");
-const clear = document.querySelector("button[type=button]");
-const modal = document.querySelector(".modal");
+const form = document.querySelector('#form');
+const cityName = document.querySelector('#city');
+const countrySelect = document.querySelector('#country');
+const button = document.querySelector('button[type=submit]');
+const geolocation = document.querySelector('button[type=geoLocation]');
+const clear = document.querySelector('button[type=button]');
+const modal = document.querySelector('.modal');
 const preloader = `<img src="./img/preloader.svg" alt="Loading..." />`;
-const container = document.querySelector(".container");
-const currentTemp = document.querySelector("#currentTemp");
-const weatherIcon = document.querySelector("#weatherIcon");
-const currentStatus = document.querySelector("#status");
-const feelsLike = document.querySelector("#feelsLike");
-const humidity = document.querySelector("#humidity");
-const wind = document.querySelector("#wind");
-const sunrise = document.querySelector("#sunrise");
-const sunset = document.querySelector("#sunset");
-const timeContainer = document.querySelector(".time");
-const updateTime = document.querySelector("#updateTime");
-const cookieTime = document.querySelector("#savedTime");
+const container = document.querySelector('.container');
+const currentTemp = document.querySelector('#currentTemp');
+const weatherIcon = document.querySelector('#weatherIcon');
+const currentStatus = document.querySelector('#status');
+const feelsLike = document.querySelector('#feelsLike');
+const humidity = document.querySelector('#humidity');
+const wind = document.querySelector('#wind');
+const sunrise = document.querySelector('#sunrise');
+const sunset = document.querySelector('#sunset');
+const timeContainer = document.querySelector('.time');
+const updateTime = document.querySelector('#updateTime');
+const cookieTime = document.querySelector('#savedTime');
 
 export function getDomVariables() {
-  return {
-    form,
-    cityName,
-    countrySelect,
-    button,
-    clear,
-    modal,
-    preloader,
-    container,
-    currentTemp,
-    weatherIcon,
-    currentStatus,
-    feelsLike,
-    humidity,
-    wind,
-    sunrise,
-    sunset,
-    timeContainer,
-    updateTime,
-    cookieTime,
-  };
+    return {
+        form,
+        cityName,
+        countrySelect,
+        button,
+        geolocation,
+        clear,
+        modal,
+        preloader,
+        container,
+        currentTemp,
+        weatherIcon,
+        currentStatus,
+        feelsLike,
+        humidity,
+        wind,
+        sunrise,
+        sunset,
+        timeContainer,
+        updateTime,
+        cookieTime,
+    };
 }
