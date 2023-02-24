55c45d518d076c2812d870a62608a5fcf76e371a
Serhii Myshko <sergeiown@gmail.com>
2023-02-24 02:32:26 +0200

    fix getLocation func

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -1,55 +1,16 @@
 'use strict';
 
-// export function getLocation() {
-//     return new Promise((resolve, reject) => {
-//         if (navigator.geolocation) {
-//             navigator.geolocation.getCurrentPosition(({ coords }) => {
-//                 const { lat, lon } = { lat: coords.latitude, lon: coords.longitude };
-//                 getCityName(lat, lon).then(() => {
-//                     resolve({ lat: lat, lon: lon });
-//                     /* Close preloader */
-//                     variables.modal.close();
-//                 });
-//             });
-//         } else {
-//             /* Close preloader */
-//             variables.modal.close();
-//             reject();
-//         }
-//     });
-// }
-
-// function getCityName(lat, lon) {
-//     const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=cf56fb7d05b9d81a18cb8aa28abe286a`;
-
-//     const timeout = new Promise((_, reject) => {
-//         setTimeout(() => {
-//             reject(new Error('Timeout while fetching location data'));
-//         }, 10000);
-//     });
-
-//     Promise.race([fetch(url), timeout])
-//         .then((response) => response.json())
-//         .then((data) => {
-//             const geoLocationCountry = data[0].country;
-//             const geoLocationCity = data[0].name;
-//             localStorage.setItem('geoLocationCountry', geoLocationCountry);
-//             localStorage.setItem('geoLocationCity', geoLocationCity);
-//         })
-//         .catch((error) => {
-//             /* show the error for 3 seconds */
-//             variables.modal.textContent = error.message;
-//             setTimeout(() => {
-//                 variables.modal.close();
-//             }, 3000);
-//         });
-// }
+import { clearForm } from './form_clear.js';
 
 export function getLocation() {
     return new Promise((resolve, reject) => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(({ coords }) => {
                 const { lat, lon } = { lat: coords.latitude, lon: coords.longitude };
+
+                if (!getCityName(lat, lon)) {
+                    clearForm();
+                }
                 getCityName(lat, lon)
                     .then(() => {
                         resolve({ lat: lat, lon: lon });
