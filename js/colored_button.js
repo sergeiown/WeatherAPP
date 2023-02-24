bdcfc07e00febf86723a70835914b5d7b6d11695
Serhii Myshko <sergeiown@gmail.com>
2023-02-24 14:34:21 +0200

    make visible buttons

--- js/colored_button.js
+++ js/colored_button.js
@@ -6,13 +6,33 @@ import { getDomVariables } from './dom_variables.js';
 const variables = getDomVariables();
 
 export function cycleButtonColors() {
-    const buttonColors = ['#1D3F72', '#5699D2', '#D8EBF9', '#71C2CC', '#4996A2', '#785471', '#E8CDC5', '#EEE7D8'];
+    const buttonColors = [
+        '#1D3F72',
+        '#2D5B8B',
+        '#3D77A5',
+        '#4D93BF',
+        '#5DAFD8',
+        '#6DCBE2',
+        '#7DE7EC',
+        '#8DFFFF',
+        '#6FF2F2',
+        '#00FBF1',
+        '#6FF2F2',
+        '#8DFFFF',
+        '#7DE7EC',
+        '#6DCBE2',
+        '#5DAFD8',
+        '#4D93BF',
+        '#3D77A5',
+        '#2D5B8B',
+        '#1D3F72',
+    ];
 
     let colorIndex = 0;
     const intervalId = setInterval(() => {
         variables.button.style.color = buttonColors[colorIndex % buttonColors.length];
         colorIndex++;
-    }, 1000);
+    }, 100);
 
     localStorage.setItem('intervalId', intervalId);
 }
