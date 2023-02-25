3ba7d9392e73962654f268933ad9f9360d559262
Serhii Myshko <sergeiown@gmail.com>
2023-02-26 00:08:35 +0200

    improve checking for old iOS

--- js/start.js
+++ js/start.js
@@ -1,11 +1,10 @@
-/* Check if the Safari browser is in use and the version of iOS. Use the navigator.userAgent. */
-
 console.clear();
 
-const uaData = navigator.userAgentData;
-const browserBrands = uaData.brands.map((brand) => brand.brand);
+if (!isIos()) {
+    /* Use userAgentData read-only property of the Navigator interface */
+    const uaData = navigator.userAgentData;
+    const browserBrands = uaData.brands.map((brand) => brand.brand);
 
-if (!isOldIOS()) {
     const script = document.createElement('script');
     script.src = './js/app.js';
     script.type = 'module';
@@ -16,18 +15,19 @@ if (!isOldIOS()) {
     script.src = './dist/app.js';
     script.type = 'module';
     document.head.appendChild(script);
-    console.log(`Outdated browser based on ${browserBrands[0]} is used`);
 }
 
-function isOldIOS() {
-    /* matches "safari" */
-    const isSafari = browserBrands.includes('Safari');
+function isIos() {
+    /* Check if the mobile browser is in use and the version of iOS is 12 and below. Use the navigator.userAgent. */
+    const whetherOldIos = () => {
+        let audio = new Audio();
+        audio.volume = 0.5;
 
-    /* matches iOS versions 1.0 through 9.9 */
-    const isOldIOS = /OS (?:1[0-9]|[1-9])_[0-9](.*) like Mac/i.test(uaData.ua);
+        return audio.volume === 1; // volume cannot be changed from "1" on iOS 12 and below
+    };
 
-    /* matches iOS versions 10.0 through 10.3 */
-    const isIOS10 = /OS 10_[0-2](.*) like Mac/i.test(uaData.ua);
+    const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isAppleDevice = navigator.userAgent.includes('Macintosh');
 
-    return isSafari && (isOldIOS || isIOS10);
+    return isIos || (isAppleDevice && whetherOldIos());
 }
